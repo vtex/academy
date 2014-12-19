bbd1e50da6b1bda2c8f19cefc4ffbdedba0b94de
Rodrigo Muniz <eu@rodrigomuniz.com>
2014-12-19 13:23:56 -0200

    Updates

--- script/main.js
+++ script/main.js
@@ -1,1 +1,2 @@
-console.log("Nice, you have passed your first test.")
+console.log("Nice, you have passed your first test. We love curious people so we love you already!")
+console.log("This page was coded by a Designer (a really nice one). So don't cha be all judgy and stuff.")
